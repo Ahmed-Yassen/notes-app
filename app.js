0ec9a19b908e05b744a6bb15be4aabd3be534106
Ahmed I. Yassen <ahmed.ibrahim.yassen@gmail.com>
2022-07-01 14:55:25 +0200

    Finish create-note functionality

--- app.js
+++ app.js
@@ -1,3 +1,23 @@
 const yargs = require("yargs");
+const notes = require("./notes");
 
-yargs.command();
+yargs.command({
+  command: "create",
+  builder: {
+    title: {
+      describe: "Enter a title for the note",
+      demandOption: true,
+      type: "string",
+    },
+    body: {
+      describe: "Enter a body for the note",
+      demandOption: true,
+      type: "string",
+    },
+  },
+  handler: (argv) => {
+    notes.createNote(argv.title, argv.body);
+  },
+});
+
+yargs.parse(); //Required to run correctly
--- notes.js
+++ notes.js
@@ -1,1 +1,38 @@
 const fs = require("fs");
+
+//-Helper functions
+const loadNotesFromFile = () => {
+  try {
+    const stringNotes = fs.readFileSync("notes.json", "utf8");
+    const jsonNotes = JSON.parse(stringNotes);
+    return jsonNotes;
+  } catch (error) {
+    //-If the file wasn't found return an empty array.
+    return [];
+  }
+};
+
+const saveNotesToFile = (notesArray) => {
+  const stringNotes = JSON.stringify(notesArray);
+  fs.writeFileSync("notes.json", stringNotes);
+};
+
+//-Shouldn't create duplicate notes
+const createNote = (title, body) => {
+  const notes = loadNotesFromFile();
+  const duplicateNotes = notes.filter((note) => {
+    return note.title === title;
+  });
+  if (duplicateNotes.length) {
+    console.log(
+      "Couldn't create note!\nYou already have a note with the same title."
+    );
+    return;
+  }
+
+  notes.push({ title, body });
+  saveNotesToFile(notes);
+  console.log("Your note was created successfully!");
+};
+
+module.exports = { createNote };
